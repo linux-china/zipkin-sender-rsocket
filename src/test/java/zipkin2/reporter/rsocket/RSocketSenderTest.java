c986918cc66e660c00e43db56f8c9a5f7161f635
linux_china <libing.chen@gmail.com>
2020-02-27 21:51:39 -0800

    code polishing

--- src/test/java/zipkin2/reporter/rsocket/RSocketSenderTest.java
+++ src/test/java/zipkin2/reporter/rsocket/RSocketSenderTest.java
@@ -6,19 +6,16 @@ import zipkin2.Callback;
 import zipkin2.Span;
 import zipkin2.codec.SpanBytesEncoder;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class RSocketSenderTest {
     private RSocketSender sender = new RSocketSender(new RSocketMock());
 
     @Test
     public void sendsSpans() throws Exception {
-        Span spanRequester = Span.newBuilder().traceId(1, 1).id(1L).build();
-        Span spanBroker = Span.newBuilder().traceId(1, 1).id(2L).parentId(1).build();
-        Span spanResponder = Span.newBuilder().traceId(1, 1).id(3L).parentId(1).build();
-        List<byte[]> encodedSpans = Stream.of(spanRequester, spanBroker, spanResponder)
+        List<byte[]> encodedSpans = spans().stream()
                 .map(SpanBytesEncoder.PROTO3::encode)
                 .collect(Collectors.toList());
         Call<Void> call = sender.sendSpans(encodedSpans);
@@ -34,6 +31,13 @@ public class RSocketSenderTest {
             }
         });
         call.execute();
+    }
+
+    public List<Span> spans() {
+        Span spanRequester = Span.newBuilder().traceId(1, 1).id(1L).build();
+        Span spanBroker = Span.newBuilder().traceId(1, 1).id(2L).parentId(1).build();
+        Span spanResponder = Span.newBuilder().traceId(1, 1).id(3L).parentId(1).build();
+        return Arrays.asList(spanRequester, spanBroker, spanResponder);
     }
 
 }
